197cd49c3d87c6aa3a141f5aacf0f8b7b222df0b
Julien Natzinger <167121754+Flopsterstream@users.noreply.github.com>
2025-06-04 15:15:11 -0700

    Update BFUtility.java

--- src/main/java/me/flopsterstream/bfutility/BFUtility.java
+++ src/main/java/me/flopsterstream/bfutility/BFUtility.java
@@ -14,18 +14,10 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class BFUtility implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("refullbright");
-	public static boolean enabled = false;
+
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Successfully loaded reFullbright");
 	}
 }
